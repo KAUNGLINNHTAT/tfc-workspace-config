5f78a7e59f3a1655f84aadbfd858a5c22ea67d6c
KAUNG LINN HTAT <kaunglinnhtat2014@gmail.com>
2024-09-28 16:38:39 +0900

    create project

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "tfe_workspace" "hcp-vault-cluster-ws" {
   name         = "hcp-vault-cluster-ws"
-  organization = data.tfe_organization.hc-linnhtat-org
+  organization = data.tfe_organization.hc-linnhtat-org.name
   tag_names    = ["hcp-vault", "app"]
   project_id = tfe_project.secureinfra_project.id
 }
 
 resource "tfe_project" "secureinfra_project" {
-  organization = data.tfe_organization.hc-linnhtat-org
+  organization = data.tfe_organization.hc-linnhtat-org.name
   name = "secureinfra-project"
 }
