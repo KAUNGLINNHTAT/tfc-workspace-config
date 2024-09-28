a15f9795203e4995a669c65a12eae5cf53452fa4
KAUNG LINN HTAT <kaunglinnhtat2014@gmail.com>
2024-09-28 17:11:29 +0900

    create iam-user-for-vault-admin-ws

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ resource "tfe_workspace" "hcp-vault-cluster-ws" {
   tag_names    = ["hcp-vault", "app"]
   project_id = data.tfe_project.default-project.id
 }
+
+resource "tfe_workspace" "iam-user-for-vault-admin-ws" {
+  name         = "iam-user-for-vault-admin-ws"
+  organization = data.tfe_organization.hc-linnhtat-org.name
+  tag_names    = ["iamuser", "app"]
+  project_id = data.tfe_project.default-project.id
+}
